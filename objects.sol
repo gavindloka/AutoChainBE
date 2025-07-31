129021ae941cea5c04d2de2c560bedcce588d810
gavindloka <lokagavindloka@gmail.com>
2025-07-31 15:04:37 +0700

    feat: transaction, contact create+read

--- objects.sol
+++ objects.sol
@@ -1,58 +1,109 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract DeAuctionObjects{
-    struct object{
-        string name; 
-        string description;
-        string email;
-        uint256 startPrice;
-        string category;
-        uint256 startDate;
-        uint256 endDate;
+contract AutoChainObjects{
+   struct CarRentalTransaction {
+        uint256 transactionId;   
+        uint256 carId;           
+        string renterName;       
+        string renterEmail;      
+        string renterProviderName;
+        string renterProviderEmail;
+        uint256 startDate;       
+        uint256 endDate;         
+        uint256 totalPrice;  
     }
 
-    object[] public Objects;
-    uint256 public TotalObjects; 
+    struct ContactMessage { 
+        uint256 contactId; 
+        string firstName; 
+        string lastName; 
+        string email; 
+        string phone; 
+        string subject; 
+        string message; 
+        uint256 timestamp; 
+    }
+
+    CarRentalTransaction[] public transactionObjects;
+    ContactMessage[] public contactMessageObjects;
+
+    uint256 public totalTransactionObjects; 
+    uint256 public totalContactMessageObjects;
 
     constructor(){
-        TotalObjects = 0;
+        totalTransactionObjects = 0;
+        totalContactMessageObjects = 0;
     }
 
-    function createObject (string memory name, string memory description, string memory email, uint256 startPrice, string memory category, uint256 startDate, uint256 endDate) public returns (uint256){
-        object memory newObject = object(name, description, email, startPrice, category, startDate, endDate);
-        Objects.push(newObject);
-        TotalObjects++;
-        return TotalObjects;
+    function createTransactionObject (
+        uint256 carId,           
+        string memory renterName,       
+        string memory renterEmail,  
+        string memory renterProviderName,
+        string memory renterProviderEmail,    
+        uint256 startDate,       
+        uint256 endDate,         
+        uint256 totalPrice  
+    ) public returns (uint256){
+        CarRentalTransaction memory newObject = 
+        CarRentalTransaction({transactionId:totalTransactionObjects, carId:carId, renterName:renterName, renterEmail:renterEmail, renterProviderName: renterProviderName, renterProviderEmail:renterProviderEmail,startDate:startDate, endDate:endDate, totalPrice:totalPrice});
+        transactionObjects.push(newObject);
+        totalTransactionObjects++;
+        return totalTransactionObjects;
     }
 
-    function updateObject (string memory email, string memory description, string memory name, uint256 startPrice,  uint256 startDate, uint256 endDate) external returns(bool){
-        for(uint i = 0; i < TotalObjects; i++){
-            if(compareStrings(Objects[i].email, email)){
-                Objects[i].name = name;
-                Objects[i].description = description;
-                Objects[i].startPrice = startPrice;
-                Objects[i].startDate = startDate;
-                Objects[i].endDate = endDate;
-                return true;
-            }
-        }
-        return false;
+    function createContactMessageObjects(
+        uint256 id,
+        string memory firstName, 
+        string memory lastName, 
+        string memory email, 
+        string memory phone, 
+        string memory subject, 
+        string memory message, 
+        uint256 timestamp
+    )public returns (uint256){
+        ContactMessage memory newObject = ContactMessage({contactId:id, firstName:firstName, lastName:lastName, email:email, phone:phone, subject:subject, message:message, timestamp:timestamp});
+        contactMessageObjects.push(newObject);
+        totalContactMessageObjects++;
+        return totalContactMessageObjects;
     }
 
-    function delObject(string memory email)external returns(bool){
-        assert(TotalObjects > 0);
-        for(uint i = 0 ; i < TotalObjects; i++){
-            if(compareStrings(Objects[i].email, email)){
-                Objects[i] = Objects[TotalObjects - 1];
-                delete Objects[TotalObjects - 1];
-                TotalObjects--;
-                return true;
-            }
-
-        }
-        return false;
+    function getAllTransactions() public view returns (CarRentalTransaction[] memory){
+        return transactionObjects;
     }
+
+    function getAllContactMessages() public view returns (ContactMessage[] memory){
+        return contactMessageObjects;
+    }
+
+    // function updateObject (string memory email, string memory description, string memory name, uint256 startPrice,  uint256 startDate, uint256 endDate) external returns(bool){
+    //     for(uint i = 0; i < TotalObjects; i++){
+    //         if(compareStrings(Objects[i].email, email)){
+    //             Objects[i].name = name;
+    //             Objects[i].description = description;
+    //             Objects[i].startPrice = startPrice;
+    //             Objects[i].startDate = startDate;
+    //             Objects[i].endDate = endDate;
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // function delObject(string memory email)external returns(bool){
+    //     assert(TotalObjects > 0);
+    //     for(uint i = 0 ; i < TotalObjects; i++){
+    //         if(compareStrings(Objects[i].email, email)){
+    //             Objects[i] = Objects[TotalObjects - 1];
+    //             delete Objects[TotalObjects - 1];
+    //             TotalObjects--;
+    //             return true;
+    //         }
+
+    //     }
+    //     return false;
+    // }
 
     
     // Helper functions
